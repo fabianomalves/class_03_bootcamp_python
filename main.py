f630e42e9af1c7e6420b472daf6b98e8099e5a81
fabianomalves <fabianomalves@proton.me>
2025-01-07 16:28:43 -0300

    Add file main.py with examples of if, elif and else

---
+++ main.py
@@ -0,0 +1,21 @@
+# # Utilization if-elif-else
+
+x = 2
+
+if x < 0:
+    x = 0
+    print('Negative changed to zero value')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+
+# Utilization from
+
+student_list = ['Fabiano', 'Bruno', 'Rafael']
+
+for student in student_list:
+    print(student)
