948b510ea662129cb37bad6feae89abedae1135b
Fabiano Alves <fabianomalves@proton.me>
2025-05-20 09:40:44 -0300

    Resolved exercise 2

--- exercise.py
+++ exercise.py
@@ -1,22 +1,35 @@
-# Exercício 1: Verificação de Qualidade de Dados
-# Você está analisando um conjunto de dados de vendas e precisa garantir
-# que todos os registros tenham valores positivos para `quantidade` e `preço`.
-# Escreva um programa que verifique esses campos e imprima "Dados válidos"
-# se ambos forem positivos ou "Dados inválidos" caso contrário.
+# # Exercício 1: Verificação de Qualidade de Dados
+# # Você está analisando um conjunto de dados de vendas e precisa garantir
+# # que todos os registros tenham valores positivos para `quantidade` e `preço`.
+# # Escreva um programa que verifique esses campos e imprima "Dados válidos"
+# # se ambos forem positivos ou "Dados inválidos" caso contrário.
 
-quantity = -1
-price = 20
+# quantity = -1
+# price = 20
 
-if quantity > 0 and price > 0:
-    print('Valid data')
-else:
-    print('Invalid data')
+# if quantity > 0 and price > 0:
+#     print('Valid data')
+# else:
+#     print('Invalid data')
 
 
 # Exercício 2: Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT.
-# Os dados incluem medições de temperatura. Você precisa classificar cada leitura
-# como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+# Os dados incluem medições de temperatura. Você precisa classificar cada
+# leitura como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+
+# Temperatura < 18°C é 'Baixa'
+# Temperatura >= 18°C e <= 26°C é 'Normal'
+# Temperatura > 26°C é 'Alta'
+
+temperature = 27
+
+if temperature < 18:
+    print('Low temperature')
+elif temperature >= 18 and temperature <= 26:
+    print('Normal temperature')
+else:
+    print('High temperature')
 
 # Exercício 3: Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens
